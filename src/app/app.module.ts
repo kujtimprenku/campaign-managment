7faeee3f50aaa31edd3fbb3508d526631d795a73
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2019-11-17 10:18:20 +0100

    Added campaign list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CampaignListComponent } from './campaign-list/campaign-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CampaignListComponent
   ],
   imports: [
     BrowserModule,
